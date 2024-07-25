00d76d332cdfac6fa75fb81db91128a9a1fca2cb
Nazish <mehakstudio26@gmail.com>
2024-07-25 07:02:13 +0500

    javascript_veriable

---
+++ statment and expression/1st.js
@@ -0,0 +1,26 @@
+//Statment & Expression 
+
+// Expression : to provide a piece of value .
+6+9;
+1947;
+
+
+true && false || !false;
+
+
+
+
+//Statment: A Block of code that to perform action 
+//statment can recieve the expression , but expression does not recieve the statment
+
+if(9 > 6 );
+
+if (78 < 98 );
+console.log(78 < 98);
+
+a=200;
+b=300;
+console.log(a+b);
+
+let a = hello;
+console.log($a);
---
+++ variabl.js
@@ -0,0 +1,42 @@
+let a=null;
+let b = 345;
+let c= true;
+let d=BigInt("567") + BigInt("3");
+ console.log(a, b, c, d);
+ console.log(typeof d);
+ console.log(typeof c);
+ console.log(typeof b);
+
+
+  a= 10;
+  b=20;
+  console.log(a, b);
+
+  a+b;
+  console.log(a+b);
+
+  c=200;
+  d=300;
+  console.log(c%d);
+
+  c=200;
+  d=300;
+  console.log(c*d);
+  console.log(typeof c*d);
+
+  c=200;
+  d=300;
+  console.log(c>d);
+
+  c=200;
+  d=300;
+  console.log(c<d);
+ 
+
+  c=200;
+  d=300;
+  console.log(c=>d);
+
+  n=1.5678;
+  console.log(n);
+ console.log(typeof n);
